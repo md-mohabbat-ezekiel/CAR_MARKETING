95636534686d94a3e4f0d621ec742dadfb1a0702
Md. Mohabbat Ezekiel <129573768+md-mohabbat-ezekiel@users.noreply.github.com>
2024-01-20 20:21:09 +0600

    Delete module 20/mid_term/phi_car_sale directory

--- module 20/mid_term/phi_car_sale/car/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("car_detail/<int:pk>/", views.CarDetail.as_view() , name="car_detail"),
-    path("buy_car/<int:pk>/", views.update_quantity , name="buy_car"),
-]
--- module 20/mid_term/phi_car_sale/comment/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Comment
-
-class AddCommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        exclude = ["car", "created_on"]
